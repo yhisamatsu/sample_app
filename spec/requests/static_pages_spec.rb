37db69195f163648b220f0450c08cdc0c4a9e30b
Y.Hisamatsu <dloglaer@yahoo.co.jp>
2012-06-18 14:38:56 +0900

    Use PostgreSQL

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_string) { "Ruby on Rails Tutorial Sample App" }
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
@@ -14,7 +14,7 @@ describe "Static pages" do
     it "should have the title 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :text => '#{base_title} | Home')
+                                :text => "#{base_title} | Home")
     end
   end
 
@@ -28,7 +28,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text => '#{base_title} | Help')
+                                :text => "#{base_title} | Help")
     end
   end
 
@@ -42,7 +42,7 @@ describe "Static pages" do
     it "should have the title 'About us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :text => '#{base_title} | About Us')
+                                :text => "#{base_title} | About Us")
     end
   end
 
@@ -56,7 +56,7 @@ describe "Static pages" do
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                                :text => '#{base_title} | Contact')
+                                :text => "#{base_title} | Contact")
     end
   end
 end
